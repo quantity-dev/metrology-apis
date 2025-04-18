c7f0549f5e9a9ceaf443b299a8ce7a0d6aed3504
andrewgsavage <andrewgsavage@gmail.com>
2025-04-18 19:17:22 +0100

    Add MetrologyNamespace (#9)
    * namespace
    
    * review
    
    * tweaks
    
    * typevars
    
    * Apply suggestions from code review
    
    Co-authored-by: Nathaniel Starkman <nstarman@users.noreply.github.com>
    
    * Update pyproject.toml
    
    * Apply suggestions from code review
    
    Co-authored-by: Nathaniel Starkman <nstarman@users.noreply.github.com>
    
    * lint
    
    * fixes
    
    * some aliases
    
    * fix errors
    
    * Update __init__.py
    
    Return MetrologyNamespace in docstirngs
    
    * Apply suggestions from code review
    
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>
    
    * Apply suggestions from code review
    
    * workaround for Python build version
    
    * CI: remove obsolete setup-python
    
    * tweak package config
    
    ---------
    
    Co-authored-by: Lucas Colley <lucas.colley8@gmail.com>
    Co-authored-by: Nathaniel Starkman <nstarman@users.noreply.github.com>
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>

--- src/metrology_apis/__init__.py
+++ src/metrology_apis/__init__.py
@@ -1,6 +1,13 @@
 """Metrology APIs."""
 
-from typing import Final, Protocol, Self, override, runtime_checkable
+from typing import (
+    Any,
+    Final,
+    Protocol,
+    Self,
+    override,
+    runtime_checkable,
+)
 
 import optype as op
 
@@ -8,8 +15,52 @@ __version__: Final = "0.0.1.dev0"
 __all__ = ["__version__", "Dimension", "Quantity", "Unit"]
 
 
+type VT = Any
+type UT = Unit[Any]
+type QT = Quantity[Any, Any, Any]
+
+
+@runtime_checkable
+class MetrologyNamespace[Q: QT = Any, V: VT = Any, U: UT = Any, D: Dimension = Any](
+    Protocol
+):
+    @staticmethod
+    def asdimension(obj: str | D) -> D: ...
+
+    @staticmethod
+    def asunit(obj: str | U) -> U: ...
+
+    @staticmethod
+    def asquantity(obj: Q | V, *, unit: U) -> Q: ...
+
+
 @runtime_checkable
 class Dimension(Protocol):
+    def __metrology_namespace__(
+        self, /, *, api_version: str | None = None
+    ) -> MetrologyNamespace[QT, VT, UT, Self]:
+        """
+        Return an object that has all the metrology API functions on it.
+
+        Parameters
+        ----------
+        api_version : str or None
+            String representing the version of the metrology API
+            specification to be returned. If it is `None`, it should
+            return the namespace corresponding to latest version of the
+            metrology API specification.  If the given version is invalid
+            or not implemented for the given module, an error should be
+            raised. Default: `None`.
+
+        Returns
+        -------
+        MetrologyNamespace
+            An object representing the metrology API namespace. It
+            should have every top-level function defined in the
+            specification as an attribute.
+        """
+        ...
+
     def __mul__(self, other: Self, /) -> Self: ...
     def __truediv__(self, other: Self, /) -> Self: ...
     def __pow__(self, other: int, /) -> Self: ...
@@ -19,9 +70,34 @@ class Dimension(Protocol):
 
 
 @runtime_checkable
-class Unit(Protocol):
+class Unit[D: Dimension = Any](Protocol):
+    def __metrology_namespace__(
+        self, /, *, api_version: str | None = None
+    ) -> MetrologyNamespace[QT, VT, Self, D]:
+        """
+        Return an object that has all the metrology API functions on it.
+
+        Parameters
+        ----------
+        api_version : str or None
+            String representing the version of the metrology API
+            specification to be returned. If it is `None`, it should
+            return the namespace corresponding to latest version of the
+            metrology API specification. If the given version is invalid
+            or not implemented for the given module, an error should be
+            raised. Default: `None`.
+
+        Returns
+        -------
+        MetrologyNamespace
+            An object representing the metrology API namespace. It should
+            have every top-level function defined in the specification as
+            an attribute.
+        """
+        ...
+
     @property
-    def dimension(self) -> Dimension: ...
+    def dimension(self) -> D: ...
 
     def __mul__(self, other: Self, /) -> Self: ...
     def __truediv__(self, other: Self, /) -> Self: ...
@@ -32,7 +108,32 @@ class Unit(Protocol):
 
 
 @runtime_checkable
-class Quantity[V, U: Unit](Protocol):
+class Quantity[V: VT = Any, U: UT = Any, D: Dimension = Any](Protocol):
+    def __metrology_namespace__(
+        self, /, *, api_version: str | None = None
+    ) -> MetrologyNamespace[Self, V, U, D]:
+        """
+        Return an object that has all the metrology API functions on it.
+
+        Parameters
+        ----------
+        api_version : str or None
+            String representing the version of the metrology API
+            specification to be returned. If it is `None`, it should
+            return the namespace corresponding to the latest version of
+            the metrology API specification. If the given version is
+            invalid or not implemented for the given module, an error
+            should be raised. Default: `None`.
+
+        Returns
+        -------
+        MetrologyNamespace
+            An object representing the metrology API namespace. It should
+            have every top-level function defined in the specification as
+            an attribute.
+        """
+        ...
+
     @property
     def value(self) -> V: ...
     @property
@@ -42,64 +143,64 @@ class Quantity[V, U: Unit](Protocol):
 
     @override
     def __eq__[B](  # type: ignore[override]  # pyright: ignore[reportIncompatibleMethodOverride]
-        self: "Quantity[V, U]", other: "Quantity[op.CanEq[V, B], U]", /
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanEq[V, B], U, D]", /
     ) -> B: ...
 
     @override
     def __ne__[B](  # type: ignore[override]  # pyright: ignore[reportIncompatibleMethodOverride]
-        self: "Quantity[V, U]", other: "Quantity[op.CanEq[V, B], U]", /
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanEq[V, B], U, D]", /
     ) -> B: ...
 
     def __lt__[B](
-        self: "Quantity[V, U]", other: "Quantity[op.CanLt[V, B], U]", /
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanLt[V, B], U, D]", /
     ) -> B: ...
     def __le__[B](
-        self: "Quantity[V, U]", other: "Quantity[op.CanLe[V, B], U]", /
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanLe[V, B], U, D]", /
     ) -> B: ...
     def __gt__[B](
-        self: "Quantity[V, U]", other: "Quantity[op.CanGt[V, B], U]", /
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanGt[V, B], U, D]", /
     ) -> B: ...
     def __ge__[B](
-        self: "Quantity[V, U]", other: "Quantity[op.CanGe[V, B], U]", /
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanGe[V, B], U, D]", /
     ) -> B: ...
 
-    def __pos__[R](self: "Quantity[op.CanPos[R], U]") -> "Quantity[R, U]": ...
-    def __neg__[R](self: "Quantity[op.CanNeg[R], U]") -> "Quantity[R, U]": ...
-    def __abs__[R](self: "Quantity[op.CanAbs[R], U]") -> "Quantity[R, U]": ...
+    def __pos__[R](self: "Quantity[op.CanPos[R], U, D]") -> "Quantity[R, U, D]": ...
+    def __neg__[R](self: "Quantity[op.CanNeg[R], U, D]") -> "Quantity[R, U, D]": ...
+    def __abs__[R](self: "Quantity[op.CanAbs[R], U, D]") -> "Quantity[R, U, D]": ...
 
     def __add__[R](
-        self: "Quantity[V, U]", other: "Quantity[op.CanRAdd[V, R], U]", /
-    ) -> "Quantity[R, U]": ...
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanRAdd[V, R], U, D]", /
+    ) -> "Quantity[R, U, D]": ...
     def __radd__[R](
-        self: "Quantity[V, U]", other: "Quantity[op.CanAdd[V, R], U]", /
-    ) -> "Quantity[R, U]": ...
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanAdd[V, R], U, D]", /
+    ) -> "Quantity[R, U, D]": ...
     def __sub__[R](
-        self: "Quantity[V, U]", other: "Quantity[op.CanRSub[V, R], U]", /
-    ) -> "Quantity[R, U]": ...
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanRSub[V, R], U, D]", /
+    ) -> "Quantity[R, U, D]": ...
     def __rsub__[R](
-        self: "Quantity[V, U]", other: "Quantity[op.CanSub[V, R], U]", /
-    ) -> "Quantity[R, U]": ...
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanSub[V, R], U, D]", /
+    ) -> "Quantity[R, U, D]": ...
 
     def __mul__[R](
-        self: "Quantity[V, U]", other: "Quantity[op.CanRMul[V, R], U]", /
-    ) -> "Quantity[R, U]": ...
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanRMul[V, R], U, D]", /
+    ) -> "Quantity[R, U, D]": ...
     def __rmul__[R](
-        self: "Quantity[V, U]", other: "Quantity[op.CanMul[V, R], U]", /
-    ) -> "Quantity[R, U]": ...
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanMul[V, R], U, D]", /
+    ) -> "Quantity[R, U, D]": ...
     def __truediv__[R](
-        self: "Quantity[V, U]", other: "Quantity[op.CanRTruediv[V, R], U]", /
-    ) -> "Quantity[R, U]": ...
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanRTruediv[V, R], U, D]", /
+    ) -> "Quantity[R, U, D]": ...
     def __rtruediv__[R](
-        self: "Quantity[V, U]", other: "Quantity[op.CanTruediv[V, R], U]", /
-    ) -> "Quantity[R, U]": ...
+        self: "Quantity[V, U, D]", other: "Quantity[op.CanTruediv[V, R], U, D]", /
+    ) -> "Quantity[R, U, D]": ...
 
     def __pow__[R](
-        self: "Quantity[op.CanPow2[op.JustInt | op.JustFloat, R], U]",
+        self: "Quantity[op.CanPow2[op.JustInt | op.JustFloat, R], U, D]",
         other: op.JustInt | op.JustFloat,
         /,
-    ) -> "Quantity[R, U]": ...
+    ) -> "Quantity[R, U, D]": ...
     def __rpow__[R](
-        self: "Quantity[op.CanRPow[op.JustInt | op.JustFloat, R], U]",
+        self: "Quantity[op.CanRPow[op.JustInt | op.JustFloat, R], U, D]",
         other: op.JustInt | op.JustFloat,
         /,
-    ) -> "Quantity[R, U]": ...
+    ) -> "Quantity[R, U, D]": ...
